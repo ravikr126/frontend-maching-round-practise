84518f34b323ea22c4930dc58b6e8cf32d3afe46
ravikr126 <ravi14577@gmail.com>
2024-09-12 19:25:14 +0530

    adding 1 more project

--- 4 C Shape With 7 Box/script.js
+++ 4 C Shape With 7 Box/script.js
@@ -21,3 +21,32 @@ boxContainer.addEventListener("click", function (e) {
     }
   }
 });
+
+
+
+//2nd method
+
+
+// const boxes = document.querySelectorAll(".box");
+// let queue = [];
+// boxes.forEach(function (box) {
+//   box.addEventListener("click", function (e) {
+//     this.classList.add("active");
+//     queue.push(this);
+//     removeActiveClassInOrder();
+//   });
+// });
+
+// function removeActiveClassInOrder() {
+//   let totalActive = document.querySelectorAll(".active");
+//   if (totalActive.length === boxes.length) {
+//     setTimeout(() => {
+//       for (let i = 0; i < queue.length; i++) {
+//         setTimeout(() => {
+//           let box = queue.shift();
+//           box.classList.remove("active");
+//         }, i * 1000);
+//       }
+//     }, 2000);
+//   }
+// }
