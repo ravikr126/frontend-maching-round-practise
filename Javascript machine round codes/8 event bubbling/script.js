b1d6eeb256e81745af3fcfcca78aab3ee7eb54b8
ravikr126 <ravi14577@gmail.com>
2024-10-02 22:02:58 +0530

    and stop propagation

--- Javascript machine round codes/8 event bubbling/script.js
+++ Javascript machine round codes/8 event bubbling/script.js
@@ -1,16 +1,17 @@
-let form= document.getElementById("form")
-let div= document.getElementById('div')
-let ptag = document.getElementById('ptag')
+let form = document.getElementById("form");
+let div = document.getElementById("div");
+let ptag = document.getElementById("ptag");
 
-form.addEventListener('click',function(e){
-    alert("form clicked")
-    console.log('Form tag clicked')
-})
-div.addEventListener('click',function(e){
-    alert("div clicked")
-    console.log('Div tag clicked')
-})
-ptag.addEventListener('click',function(e){
-    alert("P tag clicked")
-    console.log('P tag clicked')
-})
+form.addEventListener("click", function (e) {
+  alert("form clicked");
+  console.log("Form tag clicked");
+});
+div.addEventListener("click", function (e) {
+  alert("div clicked");
+  console.log("Div tag clicked");
+});
+ptag.addEventListener("click", function (e) {
+//   e.stopPropagation();
+  alert("P tag clicked");
+  console.log("P tag clicked");
+});
